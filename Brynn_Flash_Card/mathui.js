fb5f18b870a2c4be8dc4ad208bb529b4aae61e43
Melanie Wilson <madlantern@gmail.com>
2017-09-03 18:54:50 -0700

    Begin to combine four math operations onto one page. Added new general html page and mathQuiz.js as a place to store the math quiz and analyze query string for values.

--- Brynn_Flash_Card/mathui.js
+++ Brynn_Flash_Card/mathui.js
@@ -1,16 +1,35 @@
 $(document).ready(function() {
 
-    var nextURL = $("form#operationMenu input[type=radio]:checked").val();
+    var nextURL;
+    var quizURL = "mathQuiz.html";
+    var qsOperation = $("form#operationMenu input[type=radio]:checked").val();
+    var qsLevel = $("form#levelMenu input[type=radio]:checked").val();
 
     $("form#operationMenu input[type=radio]").click(function() {
-  		nextURL = $(this).val();
-  		 console.log(nextURL);
+  		qsOperation = $("form#operationMenu input[type=radio]:checked").val();
+	});
+	
+	$("form#levelMenu input[type=radio]").click(function() {
+  		qsLevel = $("form#levelMenu input[type=radio]:checked").val();
 	});
 	
 
 	$("form#operationMenu").submit(function(event) {
+  		$("#operationMenu").hide();
+  		$("#levelMenu").show();
+ 		event.preventDefault();
+	});
+	
+	
+	$("form#levelMenu").submit(function(event) {
+  		nextURL = quizURL + "?quizOperation=" + qsOperation + "&quizLevel=" + qsLevel;
   		window.open(nextURL, '_self');
  		event.preventDefault();
 	});
 
+
+	
+
 });//end document ready
+
+
